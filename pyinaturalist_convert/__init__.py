c423aece063e88bb6f902cd7c2f72f86784f5bee
Jordan Cook <jordan.cook@pioneer.com>
2022-05-11 14:46:54 -0500

    Update dependencies, and move gpxpy imports to function scope

--- pyinaturalist_convert/__init__.py
+++ pyinaturalist_convert/__init__.py
@@ -5,10 +5,6 @@ from .csv import load_csv_exports
 from .dwc import to_dwc
 from .dwca import *
 from .geojson import to_geojson
+from .gpx import to_gpx
 from .odp import download_odp_metadata
-
-# Attempt to import additional modules with optional dependencies
-try:
-    from .gpx import to_gpx
-except ImportError as e:
-    to_gpx = lambda *args, **kwargs: print(e)  # type: ignore
+from .sqlite import load_table
